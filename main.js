e4c000e3fd534e785fbbe6993d7903aa6da791ba
Assis Sousa <assissousa207@gmail.com>
2024-06-13 15:13:04 -0400

    add graficos verticais

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const labels = ["jun-23", "jul-23", "ago-23", "set-23", "out-23", "nov-23", "dez-23", "jan-24", "fev-24", "mar-24", "abr-24", "mai-24"];
+// grafico 1
 const perdasReativo = ["1497", "0", "1899", "2061", "2250", "2301", "1143", "217", "0", "408", "627", "908"];
+// grafico 2
 const reativoExcedente = ["4158", "4392", "5274", "5724", "6249", "6392", "3174", "578", "0", "1133", "1687", "2364"];
+// grafico 3
 const perdasReativoR$ = ["39056", "7377", "39226", "44859", "51715", "47949", "39870", "23642", "14525", "11103", "12517", "4333"];
 const perdasDemandaR$ = ["48479", "48557", "41774", "34869", "40229", "36538", "32506", "54694", "58214", "79787", "75174", "56937"];
 const multasOutros = ["0", "0", "0", "5965", "11769", "0", "1017", "0", "0", "0", "0", "74"];
 const totalPerdas = ["87536", "55935", "81001", "85695", "103715", "84488", "73393", "78336", "72739", "90890", "87691", "61345"];
-const unidades = ["FLORES","BV SANTA TEREZA","GRANDE CIRCULAR","PATIO ADRIANOPOLIS","PV IMIGANTE","BV EQUATORIAL","PV CANTUARIA","ALVORADA","PONTA NEGRA","BV RORAIMA"];
-const unidadesPerdasDez = [
-    "BV RORAIMA", "PONTA NEGRA","ALVORADA","PV CANTUARIA","BV EQUATORIAL","PV IMIGANTE","PATIO ADRIANOPOLIS","GRANDE CIRCULAR","BV SANTA TEREZA","FLORES"];
+// grafico 4
+const unidadesPerdasDez = ["BV RORAIMA", "PONTA NEGRA", "ALVORADA", "PV CANTUARIA", "BV EQUATORIAL", "PV IMIGANTE", "PATIO ADRIANOPOLIS", "GRANDE CIRCULAR", "BV SANTA TEREZA", "FLORES"];
+const totalPerdasDez = ["10907", "9710", "7110", "6854", "4585", "2868", "2803", "2614", "2299", "1674"];
+const PerdasReativoDez = ["0", "107", "0", "298", "1061", "3", "0", "20", "0", "56"];
+const PerdasSobraDemandaDez = ["10907", "0", "0", "6555", "3524", "2864", "2803", "2594", "2299", "1618"];
+const PerdasUtraDemandaDez = ["0", "9529", "7110", "0", "0", "0", "0", "0", "0", "0"];
+const PerdasMultasOutrosDez = ["0", "74", "0", "0", "0", "0", "0", "0", "0", "0"];
 
+
+// grafico 1
 const createChart = (id) => {
     const ctx = document.getElementById(id);
     new Chart(ctx, {
@@ -31,7 +40,7 @@ const createChart = (id) => {
         }
     });
 }
-
+// grafico 2
 const createChart1 = (id) => {
     const ctx = document.getElementById(id);
     new Chart(ctx, {
@@ -60,7 +69,7 @@ const createChart1 = (id) => {
         }
     });
 }
-
+// grafico 3
 const createChart2 = (id) => {
     const ctx = document.getElementById(id);
     new Chart(ctx, {
@@ -68,7 +77,7 @@ const createChart2 = (id) => {
         data: {
             labels: labels,
             datasets: [{
-                label: 'Perda Reativo R$',
+                label: 'Perda',
                 data: perdasReativoR$,
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.2)'
@@ -77,7 +86,7 @@ const createChart2 = (id) => {
                     'rgba(54, 162, 235, 1)'
                 ],
                 borderWidth: 1
-            },{
+            }, {
                 label: 'Perda Demanda',
                 data: perdasDemandaR$,
                 backgroundColor: [
@@ -87,7 +96,7 @@ const createChart2 = (id) => {
                     'rgba(255, 206, 86, 1)'
                 ],
                 borderWidth: 1
-            },{
+            }, {
                 label: 'Multas e Outros',
                 data: multasOutros,
                 backgroundColor: [
@@ -97,8 +106,70 @@ const createChart2 = (id) => {
                     'rgba(255, 12, 104, 1)'
                 ],
                 borderWidth: 1
-            },{
-                label: 'Tota de Perdas',
+            }, {
+                label: 'Total de Perdas',
+                data: totalPerdas,
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                tension: 0.4,
+                type: 'line'
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    stacked: true
+                },
+                y: {
+                    beginAtZero: true,
+                    stacked: true
+
+                }
+            }
+        }
+    });
+}
+// grafico 4
+const createChart3 = (id) => {
+    const ctx = document.getElementById(id);
+    new Chart(ctx, {
+        type: 'bar',
+
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Perda',
+                data: perdasReativoR$,
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Perda Demanda',
+                data: perdasDemandaR$,
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Multas e Outros',
+                data: multasOutros,
+                backgroundColor: [
+                    'rgba(255, 26, 104, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 12, 104, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Total de Perdas',
                 data: totalPerdas,
                 backgroundColor: 'rgba(153, 102, 255, 0.2)',
                 borderColor: 'rgba(153, 102, 255, 1)',
@@ -122,15 +193,16 @@ const createChart2 = (id) => {
     });
 }
 
-const createChart3 = (id) => {
+const createChart4 = (id) => {
     const ctx = document.getElementById(id);
     new Chart(ctx, {
         type: 'bar',
+
         data: {
-            labels: unidades,
+            labels: labels,
             datasets: [{
                 axis: 'y',
-                label: 'Perda Reativo R$',
+                label: 'Perda',
                 data: perdasReativoR$,
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.2)'
@@ -139,9 +211,19 @@ const createChart3 = (id) => {
                     'rgba(54, 162, 235, 1)'
                 ],
                 borderWidth: 1
-            },{
-                label: 'Perdas Demandas R$',
+            }, {
+                label: 'Perda Demanda',
                 data: perdasDemandaR$,
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: 'Multas e Outros',
+                data: multasOutros,
                 backgroundColor: [
                     'rgba(255, 26, 104, 0.2)'
                 ],
@@ -149,8 +231,8 @@ const createChart3 = (id) => {
                     'rgba(255, 12, 104, 1)'
                 ],
                 borderWidth: 1
-            },{
-                label: 'Tota de Perdas',
+            }, {
+                label: 'Total de Perdas',
                 data: totalPerdas,
                 backgroundColor: 'rgba(153, 102, 255, 0.2)',
                 borderColor: 'rgba(153, 102, 255, 1)',
@@ -168,6 +250,7 @@ const createChart3 = (id) => {
                 y: {
                     beginAtZero: true,
                     stacked: true
+
                 }
             }
         }
@@ -178,6 +261,9 @@ createChart('grafico1');
 createChart1('grafico2');
 createChart2('grafico3');
 createChart3('grafico4');
+createChart4('grafico5');
+createChart4('grafico6');
+createChart4('grafico7');
 
 $('.menu-hamburguer').click(function () {
     $('nav').slideToggle();
